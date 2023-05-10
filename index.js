49c57a51ad643875a8a76db7f22a8a2065c5e9a9
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-10 12:55:53 +0600

    delete user added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middlewere
@@ -43,6 +43,14 @@ async function run() {
         console.log('new user',user)
         const result = await userCollection.insertOne(user);
         res.send(result)
+    })
+
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log('successfully deleted',id)
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
